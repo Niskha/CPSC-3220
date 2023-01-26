23ffe2720ab11de0bf14550b0fd30f2159d8966d
Niskha <112144442+Niskha@users.noreply.github.com>
2023-01-26 16:23:23 -0500

    Create create_data.php

---
+++ hw3/create_data.php
@@ -0,0 +1,28 @@
+<!Doctype html>
+<html>
+  <head>
+    <link rel = "stylesheet" type ="text/css" href = "create_data.css">
+    <title>Create Data</title>
+  </head>
+  <body>
+    <h1>Data Generation</h1>
+    <hr/>
+    <?php
+      /*File IO and data extraction| 5 different files and formats*/
+      //domains.txt
+    
+      //first_names.csv | excel spreadsheet
+    
+      //last_names.txt
+    
+      //street_names.txt
+    
+      //street_types.txt
+    
+      /*Table Generation | generate randomized data for 25 people, people must be unique and address must be unique*/
+      // Print a heading above each array | use the <pre></pre> tag and utilize print_r()
+    
+      /*Export Data to customers.txt | Format: first_name:last_name:street_name + street_type:email(domains + '.com') | deliminator is ':'*/
+    ?>
+  </body>
+</html>
