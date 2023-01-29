dd37070023396fb75390915e52f34fa1c2d5ab00
Niskha <112144442+Niskha@users.noreply.github.com>
2023-01-28 21:29:55 -0500

    Update create_data.php

--- hw3/create_data.php
+++ hw3/create_data.php
@@ -18,7 +18,7 @@
 			if($filetype == 0){
 				$contents = file_get_contents($filename);
 				if ($homogenizer != Null){
-					$contents = str_replace($homogenizer, $delimiter,$contents);
+					$contents = str_replace($homogenizer,$delimiter,$contents);
 				}
 				$parsedfile = explode($delimiter,$contents);
 				return $parsedfile;
@@ -33,28 +33,70 @@
 			else return $parsedfile;
 		}
 		//domains.txt | "." is the deliminator
-		$domains = array();
 		$domains = parseFile("domains.txt", 0 , ".");
 		
 		//first_names.csv | excel spreadsheet
-		$first_names = array();
 		$first_names = parseFile("first_names.csv", 1);
 		
 		//last_names.txt | newline is the delimiter
-		$last_names = array();
 		$last_names = parseFile("last_names.txt",0);
 		
 		//street_names.txt | ":" and newline is the delimiter
-		$street_names = array();
 		$street_names = parseFile("street_names.txt",0,":","\r\n");
 		
 		//street_types.txt | "..;" is the delimiter\
-		$street_types = array();
 		$street_types = parseFile("street_types.txt",0,"..;");
+		
+		//Make an array fixing the domain names in domains
+		$domain = array();
+		foreach($domains as $element){
+			$domain = $element
+		}
+		
+		//Make an array uniqueIdentity() that is all of the combinations of first_names and last_names.
+		$unique_identity = array();
+		foreach($first_names as $first){
+			foreach($last_names as $last){
+				$unique_identity = $first." ".$last;
+			}
+		}
+		
+		echo count($unique_identity);
+		//Make an array uniqueAddress() that is all of the combinations of street_name and street_type.
+		$unique_address = array();
+		foreach($street_names as $name){
+			foreach($last_names as $type){
+				$unique_address = $name." ".$type;
+			}
+		}
+		
+		echo count($unique_address);
+		/*Generate Random Array of People*/
+		function generateCustomers(array $unique_identity, array $unique_address, array $domains, int $amount{
+			if(count($unique_identity) * count($unique_address) < $amount){
+				throw new Exception('Out of bounds.');
+			}
+			$customer = array();
+			$i = 0;
+			while($i!=$amount){
+				
+				$i++;
+			}
+			return $customer
+		}
+		
+		try{
+			$customer = generateCustomers();
+		} catch(Exception $e){
+			echo "Exception: ".$e->getMessage()."\n";
+		}
 		
 		/*Table Generation | generate randomized data for 25 people, people must be unique 
 		and address must be unique*/
-		// Print a heading above each array | use the <pre></pre> tag and utilize print_r()
+		// Print a heading above each array | use the pre tag and utilize print_r() for element generation
+		
+		//heading table
+		echo "<table class = 'header_table'><tr><th>First Name</th><th>Last Name</th><th>Address</th><th>Email</th></th></table>";
     
 		/*Export Data to customers.txt | 
 		Format: first_name:last_name:street_name + street_type:email(domains + '.com') | deliminator is ':'
