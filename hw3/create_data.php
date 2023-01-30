567488cfa8f8d01e10b4a21613a43a2537ecfac5
Niskha <112144442+Niskha@users.noreply.github.com>
2023-01-30 01:30:20 -0500

    Got Random generation working and added css
    Random generation now generates a random name, address, and email.
    
    added CSS to stop the flashbang

--- hw3/create_data.php
+++ hw3/create_data.php
@@ -17,6 +17,7 @@
 			// .txt file
 			if($filetype == 0){
 				$contents = file_get_contents($filename);
+				$contents = trim($contents);
 				if ($homogenizer != Null){
 					$contents = str_replace($homogenizer,$delimiter,$contents);
 				}
@@ -28,7 +29,7 @@
 				$file = fopen($filename,'r');
 				$parsedfile = fgetcsv($file);
 				fclose($file);
-				retur	n $parsedfile;
+				return $parsedfile;
 			}
 			else return $parsedfile;
 		}
@@ -37,9 +38,9 @@
 		
 		//first_names.csv | excel spreadsheet
 		$first_names = parseFile("first_names.csv", 1);
-		
+
 		//last_names.txt | newline is the delimiter
-		$last_names = parseFile("last_names.txt",0);
+		$last_names = parseFile("last_names.txt",0,"\n");
 		
 		//street_names.txt | ":" and newline is the delimiter
 		$street_names = parseFile("street_names.txt",0,":","\r\n");
@@ -48,53 +49,76 @@
 		$street_types = parseFile("street_types.txt",0,"..;");
 		
 		//Make an array fixing the domain names in domains
-		//incomplete
+		
 		$domain = array();
-		$c=0;
-		for($i = 0; $i < count($domain);i++){
-			if($c%2 = 0){
-				$domain[] = $domains[].".";
-			}
-			
-			$c++;
+		for($i = 0; $i < (count($domains)-1);$i+=2){
+			$domain[] = $domains[$i].".".$domains[$i+1];
 		}
+		
 		//Make an array uniqueIdentity() that is all of the combinations of first_names and last_names.
 		$unique_identity = array();
 		foreach($first_names as $first){
 			foreach($last_names as $last){
-				$unique_identity = $first." ".$last;
+				$last = trim($last);
+				$unique_identity[] = $first." ".$last;	
 			}
 		}
-		
-		echo count($unique_identity);
+
 		//Make an array uniqueAddress() that is all of the combinations of street_name and street_type.
 		$unique_address = array();
 		foreach($street_names as $name){
-			foreach($last_names as $type){
-				$unique_address = $name." ".$type;
+			foreach($street_types as $type){
+				$unique_address[] = $name." ".$type;
 			}
 		}
 		
-		echo count($unique_address);
 		/*Generate Random Array of People*/
-		function generateCustomers(array $unique_identity, array $unique_address, array $domains, int $amount{
-			if(count($unique_identity) * count($unique_address) < $amount){
+		function generateCustomers(array $unique_identity, array $unique_address, array $domain, int $amount){
+			//this should make sure that there is never more requested customer generation than possible to generate.
+			if(count($unique_identity) < $amount){
 				throw new Exception('Out of bounds.');
 			}
+			$max_identity = count($unique_identity)-1;
+			$max_address = count($unique_address)-1;
 			$customer = array();
 			$i = 0;
 			while($i!=$amount){
+				//random selection
+				//add identity to customer array
+				$random_identity_index = rand(0,$max_identity--);
+				$customer[] = [$unique_identity[$random_identity_index]];
 				
+				//add address to customer array
+				$random_address_index = rand(0,$max_address--);
+				$customer[] = [$unique_address[$random_address_index]];
+				
+				//add email address by concatinating identity and domain
+				$domain_index = rand(0,count($domain)-1);
+				$email = str_replace(" ",".",$unique_identity[$random_identity_index]);
+				$customer[] = [$email."@".$domain[$domain_index]];
+				//remove identity index 
+				unset($unique_identity[$random_identity_index]);
+				$unique_identity = array_values($unique_identity);
+				//remove address index
+				unset($unique_address[$random_address_index]);
+				$unique_address = array_values($unique_address);
 				$i++;
 			}
-			return $customer
+			return $customer;
 		}
 		
 		try{
-			$customer = generateCustomers();
+			$customer = generateCustomers($unique_identity,$unique_address,$domain,25);
+			foreach($customer as $data){
+				foreach($data as $info){
+					echo $info." ";
+				}
+			}
 		} catch(Exception $e){
-			echo "Exception: ".$e->getMessage()."\n";
+			echo "Exception: ".$e->getMessage();
 		}
+		
+		
 		
 		/*Table Generation | generate randomized data for 25 people, people must be unique 
 		and address must be unique*/
